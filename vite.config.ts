48317b027f779e1bd09cea7e3eb80bd5d14bc86b
Kanyoza2096 <kanyo2096@gmail.com>
2025-07-23 20:44:23 +0200

    your commit message

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,15 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/kanyozatech-creator/' : '/',
+// https://vitejs.dev/config/
+export default defineConfig({
+  // Set the base for GitHub Pages
+  base: '/kanyozatech-creator/',
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'), // Allows imports like: import x from "@/components/X"
     },
   },
   build: {
@@ -19,15 +21,15 @@ export default defineConfig(({ mode }) => ({
         assetFileNames: 'assets/[name]-[hash][extname]',
         chunkFileNames: 'assets/[name]-[hash].js',
         entryFileNames: 'assets/[name]-[hash].js',
-      }
-    }
+      },
+    },
   },
   server: {
     port: 3000,
-    strictPort: true
+    strictPort: true,
   },
   preview: {
     port: 4173,
-    strictPort: true
-  }
-}));
+    strictPort: true,
+  },
+});
